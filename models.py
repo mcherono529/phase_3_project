f21b3878de09f31fecc9d6f1cea79e9e15063364
mercy <mcherono529@gmail.com>
2024-12-19 10:22:12 +0300

    animal models

--- models.py
+++ models.py
@@ -16,3 +16,15 @@ class Habitat(Base):
         return f"Habitat(id={self.id}, name='{self.name}', type='{self.type}')"
 
 
+class Animal(Base):
+    __tablename__ = "animals"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    species = Column(String, nullable=False)
+    age = Column(Integer, nullable=False)
+    habitat_id = Column(Integer, ForeignKey("habitats.id"))
+
+    habitat = relationship("Habitat", back_populates="animals")
+
+    def __repr__(self):
+        return f"Animal(id={self.id}, name='{self.name}', species='{self.species}', age={self.age}, habitat_id={self.habitat_id})"
