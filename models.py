4db0ff5d72f8cc6c50511e9a7955ab5f94714f36
mercy <mcherono529@gmail.com>
2024-12-19 10:20:51 +0300

    models

--- models.py
+++ models.py
@@ -4,28 +4,3 @@ import sys
 
 Base = declarative_base()
 
-# Models
-class Habitat(Base):
-    __tablename__ = "habitats"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    type = Column(String, nullable=False)
-
-    animals = relationship("Animal", back_populates="habitat")
-
-    def __repr__(self):
-        return f"Habitat(id={self.id}, name='{self.name}', type='{self.type}')"
-
-
-class Animal(Base):
-    __tablename__ = "animals"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    species = Column(String, nullable=False)
-    age = Column(Integer, nullable=False)
-    habitat_id = Column(Integer, ForeignKey("habitats.id"))
-
-    habitat = relationship("Habitat", back_populates="animals")
-
-    def __repr__(self):
-        return f"Animal(id={self.id}, name='{self.name}', species='{self.species}', age={self.age}, habitat_id={self.habitat_id})"
